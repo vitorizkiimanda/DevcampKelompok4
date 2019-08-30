1b94417d2079549b0744de567eb946c63073eb2d
vitopahamify <vitorizkiimanda@pahamify.com>
2019-08-30 13:19:02 +0700

    android init

---
+++ Android/Tokofable/app/src/main/java/id/codepanda/tokofable/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.codepanda.tokofable.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import id.codepanda.tokofable.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Android/Tokofable/app/src/main/java/id/codepanda/tokofable/activity/SplashActivity.kt
@@ -0,0 +1,11 @@
+package id.codepanda.tokofable.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
